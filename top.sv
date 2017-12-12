fd61adc209277ed106155e42d2c90ee64c727da1
Wenyi Chen <wenyichen@outlook.com>
2017-12-12 01:20:25 -0800

    started working on top

--- top.sv
+++ top.sv
@@ -2,10 +2,10 @@
 // Latest rev date: 2017.11.06
 // Created by:      J Eldon
 // Design Name:     CSE141L
-// Module Name:     top (top of sample microprocessor design) 
+// Module Name:     top (top of sample microprocessor design)
 
 module top(
-  input clk,  
+  input clk,
         reset,
   output logic done
 );
@@ -17,14 +17,15 @@ module top(
   wire[IW-1:0] PC;                    // pointer to insr. mem
   wire[   8:0] InstOut;				// 9-bit machine code from instr ROM
   wire[   7:0] rt_val_o,			// reg_file data outputs to ALU
-               rs_val_o,			// 
+               rs_val_o,			//
                result_o;			// ALU data output
   wire ov_o;
   wire alu_z_o;
   logic wen_i;                      // reg file write enable
+  logic coutwen_i
   logic carry_en = 1'b1;
-  logic carry_clr;
-  assign carry_clr = reset;
+  //logic carry_clr;
+  //assign carry_clr = reset;
   logic ov_i;
   logic[7:0] alu_mux;
   wire [7:0] DataOut;
@@ -33,14 +34,16 @@ module top(
   logic[7:0] DataAddress;
 
 IF IF1(
-  .Abs_Jump (Abs_Jump)  ,   // branch to "offset"
-  .Rel_Jump (Rel_Jump)	 ,	// branch by "offset"
-  .Offset   (Offset  )	 ,
-  .Reset    (reset   )	 ,
-  .Halt     (1'b0    )	 ,
-  .CLK      (clk     )	 ,
-  .PC       (PC      )      // pointer to insr. mem
-  );				 
+  //.Abs_Jump (Abs_Jump)  ,   // branch to "offset"
+  //.Rel_Jump (Rel_Jump)	 ,	// branch by "offset"
+  //.Offset   (Offset  )	 ,
+  .reset    (reset   )	 ,
+  .halt     (1'b0    )	 ,
+  .clk      (clk     ),
+  //.PC       (PC      ),      // pointer to insr. mem
+  //TODO: need new address
+  .core       (core )
+  );
 
 InstROM #(.IW(16)) InstROM1(
   .InstAddress (PC),	// address pointer
@@ -50,30 +53,35 @@ reg_file #(.raw(3)) rf1	 (
   .clk		     (clk		    ),   // clock (for writes only)
   .rs_addr_i	 (InstOut[2:0]  ),   // read pointer rs
   .rt_addr_i	 (InstOut[5:3]  ),   // read pointer rt
-  .write_addr_i  (InstOut[2:0]  ),   // write pointer (rd)
   .wen_i		 (wen_i		    ),   // write enable
-  .write_data_i	 (rf_select     ),   // data to be written/loaded 
+  .coutwen_i		 (coutwen_i		    ),   // cout write enable
+  .write_data_i	 (rf_select     ),   // data to be written/loaded
+  .cout_data_i	 (TODO),   // cout to be written/loaded
   .rs_val_o	     (rs_val_o	    ),   // data read out of reg file
   .rt_val_o		 (rt_val_o	    )
                 );
 
+//??
 assign rf_select = rf_sel? DataOut : result_o;	// supports load commands
 
-alu alu1(.rs_i     (alu_mux)     ,	
-         .rt_i	   (rt_val_o)	  ,	
-    	 .ov_i     (ov_i)     ,	
-         .op_i	   (InstOut)	  ,	
+alu alu1(.rs_i     (alu_mux)     ,
+         .rt_i	   (rt_val_o)	  ,
+//    	 .ov_i     (ov_i)     ,
+         .op_i	   (InstOut)	  ,
 // outputs
          .result_o (result_o) ,
-		 .ov_o     (ov_o    ) ,
-		 .alu_z_o  (alu_z_o ));
+		 .carry_o     (ov_o    ) ,
+     .neg_o     (neg_o    ) ,
+     .zero_o     (zero_o    ));
+	//	 .alu_z_o  (alu_z_o ));
 
+//looks like it will work as is
 data_mem dm1(
-   .CLK           (clk        ),        
+   .CLK           (clk        ),
    .DataAddress   (DataAddress),
-   .ReadMem       (1'b1       ), // mem read always on		
-   .WriteMem      (WriteMem   ), // 1: mem_store		
-   .DataIn        (rs_val_o   ), // store (from RF)		
+   .ReadMem       (1'b1       ), // mem read always on
+   .WriteMem      (WriteMem   ), // 1: mem_store
+   .DataIn        (rs_val_o   ), // store (from RF)
    .DataOut       (DataOut    )  // load  (to RF)
 );
 
