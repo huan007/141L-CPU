10a18be80a6ac0aeb75752adfbc477078fa9daec
cyraxnguyen <cyraxnguyen@gmail.com>
2017-12-13 09:38:28 -0800

    Added Reg in PC. Initialized RegFile to 0

--- PC.sv
+++ PC.sv
@@ -9,24 +9,27 @@ module IF (
 	input [7:0] clk,
 	//input [7:0] newAddress,
 	output logic[7:0] core);
+	reg [7:0] AddressReg;
+
+	assign core = AddressReg;
 
 	always_ff @ (posedge clk)
 		if(reset)
-			core <= 0;
+			AddressReg <= 0;
 		else if (halt)
-			core <= core;
+			AddressReg <= AddressReg;
 		else if (branchsig==1'b1) //if branch instruction
 			if(branchtype==1'b0)
 				if(cmp<2'h00)
-					core <= core + BranchOut;
+					AddressReg <= AddressReg + BranchOut;
 				else
-					core <= core + 1; //blt
+					AddressReg <= AddressReg + 1; //blt
 			else
 				if(cmp!=2'h00)
-					core <= core + BranchOut;
+					AddressReg <= AddressReg + BranchOut;
 				else
-					core <= core + 1; //bne
+					AddressReg <= AddressReg + 1; //bne
 		else
-			core <= core + 1; //normal advance
+			AddressReg <= AddressReg + 1; //normal advance
 		//	core <= newAddress;
 endmodule
--- top.sv
+++ top.sv
@@ -147,4 +147,16 @@ assign             memread_i = control_signals[3];
 assign             memwrite_i = control_signals[2];
 assign             rf_sel = control_signals[1:0];
 
+//Initialize regFile
+initial begin
+	rf1.RF[0] = 8'h00;
+	rf1.RF[1] = 8'h00;
+	rf1.RF[2] = 8'h00;
+	rf1.RF[3] = 8'h00;
+	rf1.RF[4] = 8'h00;
+	rf1.RF[5] = 8'h00;
+	rf1.RF[6] = 8'h00;
+	rf1.RF[7] = 8'h00;
+end
+
 endmodule
