fb7307ab0ec180a5ffb723508763ab9226097a29
Wenyi Chen <wenyichen@outlook.com>
2017-12-14 04:33:08 -0800

    fixed bug in InstROM

--- InstROM.sv
+++ InstROM.sv
@@ -1,9 +1,9 @@
 //Instruction
 module InstROM #(parameter IW=9, DW=8) (
-	input	[IW-1:0] InstAddress,	//address pointer
+	input	[DW-1:0] InstAddress,	//address pointer
 	output logic[IW-1:0] InstOut);
 
-	logic [IW-1:0] inst_rom [2**IW];
+	logic [IW-1:0] inst_rom [2**DW];
 
 	initial
 		$readmemb("instructions.txt", inst_rom);
